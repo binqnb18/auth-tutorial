e3a2510f18f4e613366a11e36adf4f6ea81da2a7
binqnb18 <145347915+binqnb18@users.noreply.github.com>
2024-11-27 11:15:48 +0700

    database setup

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+
+import authRoutes from "./routes/auth.route.js"
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/",  (req, res) => {
-    res.send("Hello World 122223!");
-});
+app.use("/api/auth", authRoutes);
 
-app.listen(4000, () => {
-    console.log("Server is running on port 4000");
+app.listen(PORT, () => {
+    connectDB();
+    console.log("Server is running on port: ", PORT);
 });
